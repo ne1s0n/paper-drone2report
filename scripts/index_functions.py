74ecd529803bc517bb361a3c05190c3f755d3b99
filippob <filippo.biscarini@gmail.com>
2025-02-19 15:31:03 +0100

    added: new vegetation index functions

--- scripts/index_functions.py
+++ scripts/index_functions.py
@@ -6,6 +6,24 @@ Created on Wed Feb 19 12:54:54 2025
 @author: filippo
 """
 
+import numpy as np
+
+
+def HUE(img, channels):
+	"""Hue, uses red, green, blue"""
+	try:
+		red   = img[:,:,channels.index('red')]
+		green = img[:,:,channels.index('green')]
+		blue  = img[:,:,channels.index('blue')]
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return(np.arctan(
+		(2*red - green - blue) * (green - blue) / 30.5
+	)) 
+    
+
 def GLI(img, channels):
 	"""Green leaf index, uses red, green, blue"""
 	try:
@@ -20,5 +38,18 @@ def GLI(img, channels):
 		(2.0*green - red - blue) / 
 		(2.0*green + red + blue)
 	) 
-    
-    
+
+
+def NDVI(img, channels):
+	"""Normalized vegetation index, uses red, NIR"""
+	try:
+		red = img[:,:,channels.index('red')]
+		NIR = img[:,:,channels.index('nir')]
+	except ValueError:
+		#if this clause is activated it means that the requested channel(s) are not available
+		return np.nan
+	#if we get here the index can be applied to the current image
+	return(
+		(NIR - red) /
+		(NIR + red)
+	) 
