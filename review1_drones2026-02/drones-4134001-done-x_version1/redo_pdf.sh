fe970f618d4637a1283aadacfcc0a19302c23456
filippob <filippo.biscarini@gmail.com>
2026-02-25 14:35:37 +0100

    script to compile tex

---
+++ review1_drones2026-02/drones-4134001-done-x_version1/redo_pdf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#READING FROM COMMAND LINE
+FILE="$1"
+
+#cleaning all LaTeX clutter
+rm -f $FILE.aux $FILE.dvi $FILE.log $FILE.idx $FILE.ind $FILE.toc $FILE.ilg $FILE.out $FILE.bbl $FILE.blg $FILE.nav $FILE.snm $FILE.ps $FILE.pdf
+
+#doing the usual latex routine
+pdflatex $FILE
+bibtex $FILE
+pdflatex $FILE
+pdflatex $FILE
